ba2a1c46f08209b26d0bd2beb70193ba3b0aa64a
AnjaTrobec <45061263+AnjaTrobec@users.noreply.github.com>
2018-12-16 18:49:51 +0100

    popravljena datoteka
    opomba: moj računalnik mi ne push-a sam od sebe, čeprav javi da ja?

--- uvoz/mojuvoz.r
+++ uvoz/mojuvoz.r
@@ -2,44 +2,68 @@
 
 sl <- locale("sl", decimal_mark = ",", grouping_mark = ".")
 
+install.packages("tidyr")
 library(readr)
-# tabela1 <- read_csv2("podatki/poroke-st zvez-povprecna-starost-in-starost-ob-vstopu-v-1-zvezo.csv", 
-#                      col_names=c("Leto","Sklenitve zakonskih zvez - Skupaj","Prve sklenitve zakonskih zvez ženina", "Prve sklenitve zakonskih zvez neveste","Povprečna starost ženina ob sklenitvi zakonske zveze","Povprečna starost neveste ob sklenitvi zakonske zveze","Povprečna starost ženina ob sklenitvi prve zakonske zveze","Povprečna starost neveste ob prvi sklenitvi zakonske zveze"), 
-#                      na="-",skip=2,locale=locale(encoding="Windows-1250"))
+library(tidyr)
+library(reshape2)
+
+sl <- locale(encoding = "Windows-1250")
+
 
 #TABELA 1 - podatki o številu skenjenih zvez letno in povprečna starost pri vstopu v zakonsko zvezo
 tabela1 <- read.csv2("podatki/poroke-st zvez-povprecna-starost-in-starost-ob-vstopu-v-1-zvezo.csv",
-                    col.names=c("Leto","Sklenitve zakonskih zvez - Skupaj","Prve sklenitve zakonskih zvez ?enina", "Prve sklenitve zakonskih zvez neveste","Povpre?na starost ?enina ob sklenitvi zakonske zveze","Povpre?na starost neveste ob sklenitvi zakonske zveze","Povpre?na starost ?enina ob sklenitvi prve zakonske zveze","Povpre?na starost neveste ob prvi sklenitvi zakonske zveze"),
+                    col.names=c("Leto","Sklenitve zakonskih zvez - Skupaj","Prve sklenitve zakonskih zvez ženina", "Prve sklenitve zakonskih zvez neveste","Povpre?na starost ?enina ob sklenitvi zakonske zveze","Povpre?na starost neveste ob sklenitvi zakonske zveze","Povpre?na starost ?enina ob sklenitvi prve zakonske zveze","Povpre?na starost neveste ob prvi sklenitvi zakonske zveze"),
                     skip = 2,dec = ".")
-                    
-#TABELA 2 - podatki o porokah po regijah
-gorenjska <- read.csv2("podatki/poroceni-gorenjska.csv", skip=2)
-goriska <- read.csv2("podatki/poroceni-goriska.csv", skip=2)
-jugovzhodna <- read.csv2("podatki/poroceni-jugovzhodnaslo.csv", skip=2)
-koroska <- read.csv2("podatki/poroceni-koroska.csv", skip=2)
-obala <- read.csv2("podatki/poroceni-obalnokraska.csv", skip=2)
-osrednja <- read.csv2("podatki/poroceni-osrednjeslovenska.csv", skip=2)
-podravska <- read.csv2("podatki/poroceni-podravska.csv", skip=2)
-pomurska <- read.csv2("podatki/poroceni-pomurska.csv", skip=2)
-posavska <- read.csv2("podatki/poroceni-posavska.csv", skip=2)
-primorska <- read.csv2("podatki/poroceni-primorska.csv", skip=2)
-savinjska <- read.csv2("podatki/poroceni-savinjska.csv", skip=2)
-zasavska <- read.csv2("podatki/poroceni-zasavska.csv", skip=2)
+          
+
+#zakaj mi naredi pikice namesto presledkov v col.names
+
+
+          
+#TABELA 2 - podatki o porokah po starostnih skupinah
+tabela2 <- read.csv2("podatki/poroceni-starost.csv", skip=5, header = TRUE, dec = ".")
+                     #fill = TRUE, blank.lines.skip = TRUE)
+
+#kako se znebim X pred številkami in kako poimenujem prvi stolpec <- leto
+
 
 
 #TABELA 3 - podatki o razvezah
-tabela3 <- read.csv2("podatki/razveze-z-otroci-ali-brezbrez-trajanje-zveze.csv",
-                     col.names=c("Leto","Trajanje zakonske zveze pod 1 leto","Trajanje zakonske zveze 1-4 leta","Trajanje zakonske zveze 5-9 let","Trajanje zakonske zveze 10-14 let","Trajanje zakonske zveze 15 let ali več","Razveze zakonskih zvez z otroki","Razveze zakonskih zvez brez otrok"), skip=2, dec = ".")
+tabela3 <- read.csv2("podatki/razveze-z-otroci-ali-brezbrez-trajanje-zveze.csv", skip=2, header = TRUE, dec = ".")
+                     
 
 
 #TABELA 4 - podatki o porokah med istospolnimi partnerji
-tabela4 <- read.csv2("podatki/zveze-sklenjene-med-istospolnimi-partnerji.csv",
-                     col.names=c("Leto", "med moškima", "med ženskama"), skip=2)
+tabela4 <- read.csv2("podatki/zveze-sklenjene-med-istospolnimi-partnerji.csv", skip=2, header = TRUE)
+
+#kako dobim šumnike, poznam samo locale = locale(encoding = "Windows-1250"), kar pa tukaj ne deluje
+
+
 
 #TABELA 5 - razvezani po starostnih skupinah
-tabela5 <- read.csv2("podatki/razvezani-starost.csv",
-                     col.names=c("Leto","2011","2012","2013","2014","2015","2016","2017","2018"), skip=2)
+tabela5 <- read.csv2("podatki/razvezani-starost.csv", header = TRUE, skip=2)
+
 
 #TABELA 6 - vdoveli
-tabela6 <- read.csv2("podatki/vdoveli.csv",
-                     col.names=c("Leto","2011","2012","2013","2014","2015","2016","2017","2018"), skip=6)
+tabela6 <- read.csv2("podatki/vdoveli.csv", header = TRUE, skip=6)
+
+
+
+
+
+
+# #TABELA 2 - podatki o porokah po regijah - imam 12 tabel, za vsako regijo posebej - kako naj jih spravim v eno?
+
+# gorenjska <- read.csv2("podatki/poroceni-gorenjska.csv", skip=2)
+# goriska <- read.csv2("podatki/poroceni-goriska.csv", skip=2)
+# jugovzhodna <- read.csv2("podatki/poroceni-jugovzhodnaslo.csv", skip=2)
+# koroska <- read.csv2("podatki/poroceni-koroska.csv", skip=2)
+# obala <- read.csv2("podatki/poroceni-obalnokraska.csv", skip=2)
+# osrednja <- read.csv2("podatki/poroceni-osrednjeslovenska.csv", skip=2)
+# podravska <- read.csv2("podatki/poroceni-podravska.csv", skip=2)
+# pomurska <- read.csv2("podatki/poroceni-pomurska.csv", skip=2)
+# posavska <- read.csv2("podatki/poroceni-posavska.csv", skip=2)
+# primorska <- read.csv2("podatki/poroceni-primorska.csv", skip=2)
+# savinjska <- read.csv2("podatki/poroceni-savinjska.csv", skip=2)
+# zasavska <- read.csv2("podatki/poroceni-zasavska.csv", skip=2)
+
