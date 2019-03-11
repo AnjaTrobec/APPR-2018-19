9ff20cacb24ff244fa4d03fcd979340486549ba4
AnjaTrobec <aanja.trobec@gmail.com>
2019-03-11 17:01:52 +0100

    skušam usposobiti shiny

--- shiny/mojshiny.R
+++ shiny/mojshiny.R
@@ -1,54 +1,51 @@
-library(shiny)
-
-#učenje uporabe
-
-ui <- fluidPage(
-  sliderInput(inputId = "num",
-              label = "Choose a number",
-              value = 25, min = 1, max = 100),
-  plotOutput("hist")
-)
-
-server <- function(input, output) {
-  output$hist <- renderPlot({
-    hist(rnorm(input$num))
-  })
-  
-}
-
-shinyApp(ui = ui, server = server)
+# library(shiny)
+# 
+# #učenje uporabe
+# 
+# ui <- fluidPage(
+#   sliderInput(inputId = "num",
+#               label = "Choose a number",
+#               value = 25, min = 1, max = 100),
+#   plotOutput("hist")
+# )
+# 
+# server <- function(input, output) {
+#   output$hist <- renderPlot({
+#     hist(rnorm(input$num))
+#   })
+#   
+# }
+# 
+# shinyApp(ui = ui, server = server)
 
 
 #za moj program
 
 library(shiny)
 library(readr)
-
 source("uvoz/mojuvoz.R")
 
 
 ui <- fluidPage(
   titlePanel("Poroke po regijah"),
-  uiOutput("izborTabPanel")
+  uiOutput("poroke")
 )
 
+
 server <- function(input, output) {
-  output$izborTabPanel = renderUI({
+  output$poroke = renderUI({
     tabPanel("Graf",
              sidebarPanel(
                selectInput("regija", label = "Izberite regijo:",
                            choices= (sort(unique(poroceni$regija))))),
-             mainPanel(plotOutput("poroke")))
+             mainPanel(plotOutput("poroke1")))
   })
-  
-  output$poroke <- renderPlot({
-    tabela__1 <- poroceni %>% filter(regija == input$regija)
-    print(ggplot(tabela__1) + geom_line(aes(x = leto, y = stevilo), group=1) + 
-            ylab("Poroke") + xlab("Leto") +
+  output$poroke1 <- renderPlot({
+    izbrana <- poroceni %>% filter(regija == input$regija)
+    print(ggplot(izbrana) + geom_line(aes(x = leto, y = stevilo), group=1) + ylab("Poroke") + xlab("Leto") +
             ggtitle("Poroke po regijah"))
   }) 
 }
 
 shinyApp(ui = ui, server = server)
-
 
